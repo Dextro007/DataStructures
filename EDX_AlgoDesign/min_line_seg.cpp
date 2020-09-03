f1411529ff128c0056dbef571bac449626dc7e7b
Vivek Kumar <vivek.kumar007@gmail.com>
2020-09-03 21:25:24 +0530

    added greedy - line segment

---
+++ EDX_AlgoDesign/min_line_seg.cpp
@@ -0,0 +1,19 @@
+/*
+Prblm :  You are responsible for collecting signatures from all tenants of a certain build-ing.
+        For each tenant, you know a period of time when he or she is at home.You would like to collect all signatures by visiting
+        the building as few times aspossible.
+Mathematical model: given a setof segments on a line and your goal is to mark as few points on a line as possibleso that each segment
+        contains at least one marked point
+
+DS/algo used : array/vector
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+
+  return 0;
+}
