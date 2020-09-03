73f602427a19bc205919899c695fa9e7a9dd7e77
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-09-03 22:15:47 +0530

    some update for line segment code

--- EDX_AlgoDesign/min_line_seg.cpp
+++ EDX_AlgoDesign/min_line_seg.cpp
@@ -6,18 +6,30 @@ Mathematical model: given a setof segments on a line and your goal is to mark as
         contains at least one marked point
 
 DS/algo used : array/vector
+SOln: 1. sort the points acc based on 1st coordinate
+      2. crawl form 1 segment to other by changing x2 coordinate
+      3. IF new x1 co-ord > existing x2 coordinate THEN new segments
+      4. ELSE update the x2 coordinate to biggest x2
 */
 
 #include<iostream>
 #include<algorithm>
+#include<utility>
 #include<vector>
 using namespace std;
 
 int main() {
-  vector<long> vec;
-  int n= 200;
+  vector< pair <long,long>> points;
+  long arr_x1[100], arr_x2[100];
+  int n= 100;
   for(int i = 0;i<n; i++){
-    cin>>vec[i];
+    cin>>arr_x1[i]>>arr_x2[i];
   }
+  for(int i=0; i<n; i++){
+    points.push_back(make_pair(arr_x1[i], arr_x2[i]));
+  }
+  make_heap(points.begin(), points.end());
+  sort_heap(points.begin(), points.end());
+  
   return 0;
 }
