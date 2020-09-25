da85aac1f92ce9bdf9159e2e29702e1914e7eab9
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-09-25 12:30:46 +0530

    added Sparse Array but T.C = O(n^2)

--- HK_Problem_Solving/sparse_aaray.cpp
+++ HK_Problem_Solving/sparse_aaray.cpp
@@ -1,16 +1,34 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
 
-int main(){
-  string str1 = "ab";
-  char a = 'A';
-  if(str1 == string(1, a)){
-    cout<<"Same \n";
+void sparse_array(string strings[], string queries[], int results[], int n, int q){
+  for(int i = 0; i<q; i++){
+    for(int j = 0; j<n; j++){
+      if(queries[i] == strings[j])
+        results[i]++;
+    }
+    //cout<<results[i]<<"\n";
   }
-  else{
-    cout<<"Different \n";
+  return;
+}
+
+int main(){
+  int n,q;
+  cin>>n;
+  string strings[n];
+  for(int i = 0; i<n; i++){
+    cin>>strings[i];
+  }
+  cin>>q;
+  int results[q] = {0};
+  string queries[q];
+  for(int i=0; i<q; i++){
+    cin>>queries[i];
+  }
+  sparse_array(strings, queries, results, n, q);
+  for(int i =0; i<q; i++){
+    cout<<results[i]<<"\n";
   }
   return 0;
 }
