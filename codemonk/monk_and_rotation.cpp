b5ff819b4577b5398ff4291c7c2ce765957eebff
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-09-26 23:35:23 +0530

    Monk and rotation in Progress

---
+++ codemonk/monk_and_rotation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void right_rotation(long arr[], long rotations, long new_array[], long size){
+
+}
+
+int main(){
+  int t;
+  long rotations,n;
+  for(int i = 0; i<t; i++){
+    cin>>n;
+    cin>>rotations;
+  }
+  return 0;
+}
