88c6459586afb90d6b7554f2fba702b1259511e8
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-12-03 23:03:53 +0530

    started Trees.

---
+++ Trees/basic_operations.c
@@ -0,0 +1,17 @@
+/*
+Trees can be implemented using:
+    1. dynamically created nodes
+    2. arrays:
+      for arrays node at index i:
+        left child  = 2i + 1
+        right child  = 2i + 2
+        NOTE: giving that we traverse level wise.
+*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  return 0;
+}
---
+++ Trees/basic_operations.h
@@ -0,0 +1,13 @@
+#ifndef BASIC_OPERATION
+#define BASIC_OPERATION\
+
+struct Tree{
+  int data;
+  struct Tree *left;
+  struct Tree *right;
+};
+
+extern struct Tree *root;
+
+
+#endif
