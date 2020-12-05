18c170149f6f9e87ee2f1db9301b4572a5749680
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-12-05 18:19:11 +0530

    struggling

--- Trees/basic_operations.c
+++ Trees/basic_operations.c
@@ -14,15 +14,27 @@ Trees can be implemented using:
 #include "basic_operations.h"
 // declaring all the static functions needed for a BST.
 static void Init();
-static unsigned char isEmpty();
-static void insert_node();
+// static unsigned char isEmpty();
+static void insert_node(long data);
 /*------------------------------------------------------------------*/
+
 static void Init(){
+  struct Tree *root = (struct Tree*)malloc(sizeof(struct Tree)) ;
   root = NULL;
+  return;
+}
+static void insert_node(long data){
+  struct Tree *new_node = (struct Tree*)malloc(sizeof(struct Tree));
+  new_node->data = data;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  root = new_node;
+  // printf("Root data is: %d \n", root->data);
 }
 
 int main(){
   Init();
-
+  insert_node(10);
+  printf("Hello");
   return 0;
 }
