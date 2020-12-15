4c0def52f07313635dc7fdc6555a5c1239e0004e
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-12-15 20:58:08 +0530

    completed inorder, preorder and postorder traversal

--- Trees/traversal.c
+++ Trees/traversal.c
@@ -10,7 +10,7 @@ struct Tree{
 struct Tree *root;
 //****************************--functions---**********************
 static struct Tree* create_node(long data);
-static struct Tree* insert_node(struct Tree*, long data);
+static struct Tree* insert_node(struct Tree *root , long data);
 static void inorder(struct Tree* root);
 static void preorder(struct Tree* root);
 static void postorder(struct Tree* root);
@@ -23,29 +23,69 @@ static struct Tree* create_node(long data){
   new_node->right = NULL;
 }
 
-static struct Tree* insert_node(struct Tree*, long data){
+static struct Tree* insert_node(struct Tree* root, long data){
   if(root == NULL){
     root = create_node(data);
     return root;
   }
-  if(data <= root->data){
-    root = insert_node(root->left, data);
+  else if(data <= root->data){
+    root->left = insert_node(root->left, data);
   }
   else{
-    root  = insert_node(root->right, data);
+    root->right  = insert_node(root->right, data);
   }
   return root;
 }
 
-void inorder(struct Tree* root){
+static void inorder(struct Tree* root){
   if(root == NULL)
     return;
+  // traversing the left nodes
   inorder(root->left);
   printf("%ld\t", root->data);
+  // trsaversing the right nodes
   inorder(root->right);
   return;
 }
+
+static void preorder(struct Tree *root){
+  if(root == NULL){
+    return;
+  }
+  printf("%ld\t", root->data);
+  preorder(root->left);
+  preorder(root->right);
+  return;
+}
+
+static void postorder(struct Tree *root){
+  if(root == NULL){
+    return;
+  }
+  postorder(root->left);
+  postorder(root->right);
+  printf("%ld\t", root->data);
+  return;
+}
+
 int main(){
+  root = NULL;
+  root = insert_node(root, 10);
+  root = insert_node(root, 12);
+  root = insert_node(root, 8);
+  root = insert_node(root, 13);
+  root = insert_node(root, 7);
+  root = insert_node(root, 11);
+  root = insert_node(root, 9);
+  printf("Inorder: ");
+  inorder(root);
+  printf("\n");
+  printf("Preorder: ");
+  preorder(root);
+  printf("\n");
+  printf("Postorder: ");
+  postorder(root);
+  printf("\n");
 
   return 0;
 }
