8f04da66d88d8b41bf5e739f8e54c6f63b760ee8
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-12-20 23:51:16 +0530

    still working on deletion

--- Trees/deletetion.c
+++ Trees/deletetion.c
@@ -3,6 +3,7 @@ Aim : delete the required node
 */
 #include<stdio.h>
 #include<stdlib.h>
+#define COUNT 10
 
 struct Tree{
   long data;
@@ -15,6 +16,7 @@ static struct Tree* create_node(long data);
 static struct Tree* insert_node(struct Tree* root, long data);
 static struct Tree* search(struct Tree* root, long data);
 static void delete_node(long data);
+void print2Dtree(struct Tree *root, unsigned long space);
 //*****************************************************
 static struct Tree* create_node(long data){
   struct Tree *new_node = (struct Tree*)malloc(sizeof(struct Tree));
@@ -24,6 +26,33 @@ static struct Tree* create_node(long data){
   return new_node;
 }
 
+static Tree* insert_node(struct Tree *root, long data){
+  if(root == NULL){
+    root = create_node(data);
+    return root;
+  }else if(data <= root->data){
+    root->left = insert_node(root->left, data);
+  }
+  else{
+    root->right = insert_node(root->right, data);
+  }
+  return root;
+}
+void print2Dtree(struct Tree *root, unsigned long space){
+  if(root == NULL){
+    return;
+  }
+  space += COUNT;
+  // right most element are to be printed 1st
+  print2Dtree(root->right, space);
+  for(unsigned long i = COUNT; i< space ; i++){
+    printf(" ");
+  }
+  // after right comes the parent node
+  printf("%ld", root->data);
+  // after right print left
+  print2Dtree(root->left, space);
+}
 
 int main(){
   root = NULL;
