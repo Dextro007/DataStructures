5c9b02c888b5c8be99042131175777360b0a4eb8
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-01-14 23:03:28 +0530

    graph implemented and working fine

--- Graph/graph_implementation.c
+++ Graph/graph_implementation.c
@@ -39,8 +39,6 @@ void add_linked_vertex(struct graph *graph, long source, long destination){
   struct adj_vertices *adjacent_vertex2 = create_new_node(source);
   adjacent_vertex2->next = graph->v_array[destination].head;                     // notice the use of -> and .
   graph->v_array[destination].head = adjacent_vertex2;
-  //add_linked_vertex(graph, destination, source)
-
 }
 
 struct graph* createGraph(long Vertices){
@@ -54,17 +52,14 @@ struct graph* createGraph(long Vertices){
 }
 
 void print_graph(struct graph *graph){
-  int v;
+  long v;
   struct adj_vertices *traverse = NULL;
   for(v=0; v< graph->vertices; v++){
     traverse = graph->v_array[v].head;
-    //check if there is only one element in the list
-    if(traverse->next == NULL){
-      printf("(%ld)\n", traverse->linked_vertex);
-      continue;
-    }
-    while(traverse->next != NULL){
-      printf("(%ld)->", traverse->linked_vertex);
+    // while we have a non NULL traversal value
+    printf("Adjacent vertices to %ld is\n (%ld)", v,v);
+    while(traverse){
+      printf("->(%ld)", traverse->linked_vertex);
       traverse = traverse->next;
     }
     printf("\n");
@@ -76,11 +71,11 @@ int main(){
   struct graph *g1 = createGraph(Vertices);
   add_linked_vertex(g1, 0,1);
   add_linked_vertex(g1, 0,4);
-  add_linked_vertex(g1,1,4);
+  add_linked_vertex(g1, 1,4);
   add_linked_vertex(g1, 1,2);
   add_linked_vertex(g1, 1,3);
   add_linked_vertex(g1, 2,3);
-  add_linked_vertex(g1,3,4);
-  // add_linked_vertex(g1)
+  add_linked_vertex(g1, 3,4);
+  print_graph(g1);
   return 0;
 }
