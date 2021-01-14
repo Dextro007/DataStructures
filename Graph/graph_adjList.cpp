f56974b4df1d88237e9cfb98a62b3d1176dc299d
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-01-14 23:17:04 +0530

    implementing graph in C++

--- Graph/graph_adjList.cpp
+++ Graph/graph_adjList.cpp
@@ -1,1 +1,16 @@
+/*
+AIM:              Implementing graphs in C++
+Approach :        construction of graphs in C++ is quite simpleby using vector
+                  As it is very simple to create dynamic list in C++ using vector
+*/
+
 #include<iostream>
+#include<vector>
+using namespace std;
+
+void add_vertices(vector<long> vertex_list[], long source, long destination);
+
+
+int main(){
+  return 0;
+}
