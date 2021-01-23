0c2543b9f196e5c38ad9e98dcb93140a4d9a2686
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-01-23 14:48:01 +0530

    implementing stack using arrays

---
+++ Stack/stack_implementation.c
@@ -0,0 +1,19 @@
+/*
+AIM         : Implementation of stacks
+Approach    : Using arrays
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+struct Stack{
+  long top;
+  unsigned long capacity;
+  long *array;
+};
+
+int main(){
+
+  return 0;
+}
