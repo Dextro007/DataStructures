286b6edf9eff8d3dd6954de8589f7f45f9fda019
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-01-25 16:06:43 +0530

    implementinmg basic functionality of stack

--- Stack/stack_implementation.c
+++ Stack/stack_implementation.c
@@ -12,7 +12,23 @@ struct Stack{
   unsigned long capacity;
   long *array;
 };
+//-----------------------------FUNCTIONS-DECLARATION----------------------------
+void push(struct Stack *stack, long data);
+long pop(struct Stack *stack);
+unsigned char isEmpty(struct Stack *stack);
+unsigned char isFull(struct Stack *stack);
+static struct Stack* create_stack(unsigned long capacity);
+//----------------------------*****************---------------------------------
 
+// function to create and initialize the structure parameters
+static struct Stack* create_stack(unsigned long capacity){
+  struct Stack* new_stack = (struct Stack*)malloc(sizeof(struct Stack));
+  new_stack->capacity = capacity;
+  //since 1st element in the stack will be at index 0;
+  new_stack->top = -1;
+}
+
+// function to check if the 
 int main(){
 
   return 0;
