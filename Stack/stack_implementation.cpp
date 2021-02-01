93b5ada810e7f10f752d6de62ddf5da4dce779c3
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-02-01 22:52:32 +0530

    added constructor to initialize stack parameter

--- Stack/stack_implementation.cpp
+++ Stack/stack_implementation.cpp
@@ -2,15 +2,20 @@
 
 using namespace std;
 
-class stack{
+class Stack{
 private:
   long top;
   unsigned long capacity;
   long *array;
 public:
+  // constructor to initialize the parameters
+  Stack(unsigned long stack_capacity){
+    top = -1;
+    capacity = stack_capacity;
+    array = new long[stack_capacity];
+  }
   bool push(long data);
   bool pop();
   long view_top();
   bool isEmpty();
 };
-bool Stack::push()
