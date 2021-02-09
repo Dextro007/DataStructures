728ba881b036cfdded8180ae3dc664fe8f23242b
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-02-09 21:32:07 +0530

    creating queue

--- Queue/queue_imnplementation.c
+++ Queue/queue_imnplementation.c
@@ -4,9 +4,9 @@ Approach :        using arrays
 */
 
 #include<stdio.h>
-
+#include<stdlib.h>
 struct Queue{
-  unsigned long capacity;
+  unsigned long length;
   long head;
   long tail;
   long *array;
@@ -23,7 +23,16 @@ enum queueStates isfull(struct Queue* q);
 enum queueStates isEmpty(struct Queue* q);
 long push_back(struct Queue* q, long data);
 long pop_front(struct Queue* q);
+struct Queue* createQueue(unsigned long queue_length);
+//-----------------------------------------------------------
 
+struct Queue* createQueue(unsigned long queue_length){
+  struct Queue *new_queue = (struct Queue*)malloc(sizeof(struct Queue));
+}
+
+enum queueStates isfull(struct Queue* q){
+
+}
 
 int main(){
 
