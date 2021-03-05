72dcca1f9ca0eb7a82a59ce35486cdfac001f5cb
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-03-05 23:56:11 +0530

    bfs

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -2,12 +2,14 @@
 #include<list>
 
 using namespace std;
-// construction of graph can change but aLGO 
+// construction of graph can change but aLGO
 class Graph{
   long V;
   list<int> *adj;
 public:
-  Graph(int V);
+  Graph(long V);
+  void add_vertices(long sources, long destination);
+  void bfs(long start);
 };
 
 int main(){
