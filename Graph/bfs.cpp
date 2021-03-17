cad5504105a7cc258f79a947e963a46bb582b901
Vivek Kumar <vivek.kumar007656@gmail.com>
2021-03-17 11:18:28 +0530

    using list STL in Cpp

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
-
+#include<list>
 using namespace std;
 
 class Graph{
   unsigned long V;
   list<long> *adjacency_list;
 public:
-  Graph(unsigned long Vertices)
+  Graph(unsigned long Vertices);
 };
