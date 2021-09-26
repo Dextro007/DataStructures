32edce438b8f6897cb3baaef39198d4d24bf6a08
Dextro007 <vivek.kumar007656@gmail.com>
2021-09-26 20:34:58 +0530

    Heap inprogress constructor created

--- HK_Problem_Solving/qheap.cpp
+++ HK_Problem_Solving/qheap.cpp
@@ -1,5 +1,11 @@
 /*
 Prblm  : perfrom basic opertaions on the heap
+There are 3 types of query:
+
+"1 v" - Add an element v to the heap.
+"2 v" - Delete the element v from the heap.
+"3" - Print the minimum of all the elements in the heap.
+
 Soln  : Construct a min heap or maxheap
       Heaps are conmstructed using arrays as it is a complete binary tree
 How is Binary Heap represented?
@@ -14,22 +20,45 @@ Arr[(2*i)+2]	Returns the right child node
 #include<iostream>
 
 using namespace std;
-
+/*
+Note that in heap we can insert at leftmost node
+and delete only at root.
+*/
 class minHeap{
   int capacity;
-  int *arr;
-  int size;
+  int *HeapArr;
+  int Hsize;
 public:
   // constructor  for initialization
   minHeap(int capacity);
   void minheapify(int);
-
+  // kto return the parent node of i
   int parent(int i){
     return (i-1)/2;
   }
+  // for returning the left child of the node i
   int left(int i){ return (2*i)+1; }
+  // for returning the right child of the node i
+  int right(int i){return (2*i)+2;}
+  // to extract the root(minimum element of the heap)
   int extractMin();
+  // since deletion is allowed only at root decreaseKey
+  // replaces the key to eb delete with INT_MIN and then heapifies that
+  void decreaseKey(int i);
+
+  void insertKey(int i);
+  int getMin(){
+    return HeapArr[0]
+  }
+
 };
+// constructor created
+minHeap::minHeap(int cap){
+  Hsize = 0;
+  capacity = cap;
+  HeapArr = new int[capacity];
+}
+
 
 int main()
 {
