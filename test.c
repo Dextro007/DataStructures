176b942965a45a8cedc6a2fdc20ac4d5841d9b5c
uic21796 <vivek.kumar@continental-corporation.com>
2021-12-08 16:05:29 +0530

    just practice

--- test.c
+++ test.c
@@ -1,14 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// static unsigned int counter = 0;
+struct Node{
+	int data;
+	struct Node* link;
+};
 
-void test_func(){
-  short signed int a = 12;
-  printf("%u", (unsigned short int)(~a+1));
+struct Node* head;
+
+static unsigned long listLength = 0;
+
+enum state{
+	true=0,
+	false,
+};
+
+
+struct Node* createNode(int data){
+	
+	struct Node* newNode  = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = data;
+	newNode->link = NULL;
+	return newNode;
+}
+// to insert at the end
+void insert(int data, int n){
+	struct Node* newNode = createNode(data);
+	// temp node
+	struct Node* temp = head;
+	// insert at beginning 
+	if(n==0){
+		// check if list is Empty;
+		if(listLength == 0){
+			head = newNode;
+		}
+		else{
+			newNode->link = head;
+			head = newNode;
+		}
+		listLength++;
+	}
+	else if(n>0){
+		int cnt = 0;
+		while((cnt!=n) && temp!=NULL){
+			temp = temp->link;
+		}
+		newNode->link = temp->link;
+		temp->link = newNode;
+		listLength++;
+	}
+	else{
+		printf("Invalid n\n");
+	}
 }
 
-int main(){
-  test_func();
-  return 0;
+int deleteNode(int data){
+	int retVal;	
+	if(head == NULL){
+		printf("Linked List Empty");
+		retVal = -1;
+	}
+	else{
+		struct Node* current = head;
+		struct Node* prev = NULL;
+		while(current != NULL){
+				if(current->data == data){
+						prev->link = current->link;
+						free(current);
+						retVal = current->data;
+						break;
+				}
+				prev = current;
+				current = current->link;
+		}
+	}
+	return retVal;
 }
+
+
+
+
